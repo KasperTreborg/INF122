e101e00598dc3f554b8798008f4a6a91dab32a60
KasperTreborg <kaspertreborg@outlook.com>
2023-12-01 11:33:13 +0100

    Jobb Jobb

--- EksamensOving.hs
+++ EksamensOving.hs
@@ -1,2 +1,9 @@
 module EksamensOving where
-    
+
+-- UKE 35
+---------------------------------------------------------------------------------------------------------
+f :: Integer -> Integer
+f n = n * (n + 1) `div` 2
+
+triangleNumber :: Integer -> Integer
+triangleNumber n = undefined
